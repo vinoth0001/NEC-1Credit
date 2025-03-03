34e634a2612de627204de800d3060da1b97590bc
vinoth0001 <136877910+vinoth0001@users.noreply.github.com>
2025-03-03 22:53:28 +0530

    Update personal-api.py

--- personal-api.py
+++ personal-api.py
@@ -1,4 +1,18 @@
-Update this file to add your code
-/name - Should give your name
-/register_number - Should give your register_number
-/department - Should give your department
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/name')
+def name():
+    return "VINOTH A"
+
+@app.route('/reg-no')
+def register_number():
+    return "22IT054"
+
+@app.route('/dept')
+def department():
+    return "Information Technology"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
